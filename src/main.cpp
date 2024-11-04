b963e36d3aad238eb00cf26cf8f131650b54d509
tonio-m <github@mtonio.com>
2024-11-04 10:46:30 -0300

    feat: enhance piano functionality with audio, presets and UI improvements
    - Add piano sound playback functionality with pitch shifting
    - Implement chord progression presets for ii-V-I patterns in all keys
    - Add looping option for chord progressions
    - Improve visual feedback with different colors for active/target notes
    - Add dropdown list for selecting progression presets
    - Implement PC keyboard controls for testing
    - Clean up code organization and add proper resource cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,38 @@
 #include <sstream>
 #include <RtMidi.h>
 #include <raylib.h>
+#include <algorithm>
 #define RAYGUI_IMPLEMENTATION
 #include "../include/raygui/raygui.h"
 
+
+typedef std::vector<int> Chord;
 std::array<bool, 36> PIANONOTESACTIVE;
 enum Scene { SCENE_INPUT, SCENE_PIANO };
+static std::array<std::string, 12> NOTE_SYMBOLS = {"c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"};
+std::map<std::string, std::array<char, 256>> OPTIONS = {
+    {"Cmaj/Amin ii-V-I",  {"Dmin7,G7,Cmaj7,Fmaj7,Bmin7b5,E7,Amin7"}},
+    {"Fmaj/Dmin ii-V-I",  {"Gmin7,C7,Fmaj7,Bbmaj7,Emin7b5,A7,Dmin7"}},
+    {"Bbmaj/Gmin ii-V-I", {"Cmin7,F7,Bbmaj7,Ebmaj7,Amin7b5,D7,Gmin7"}},
+    {"Ebmaj/Cmin ii-V-I", {"Fmin7,Bb7,Ebmaj7,Abmaj7,Dmin7b5,G7,Cmin7"}},
+    {"Abmaj/Fmin ii-V-I", {"Bbmin7,Eb7,Abmaj7,Dbmaj7,Gmin7b5,C7,Fmin7"}},
+    {"Dbmaj/Bbmin ii-V-I",{"Ebmin7,Ab7,Dbmaj7,Gbmaj7,Cmin7b5,F7,Bbmin7"}},
+    {"Gbmaj/Ebmin ii-V-I",{"Abmin7,Db7,Gbmaj7,Cbmaj7,Fmin7b5,Bb7,Ebmin7"}},
+    {"Bmaj/Abmin ii-V-I", {"C#min7,F#7,Bmaj7,Emaj7,Bbmin7b5,Eb7,Abmin7"}},
+    {"Emaj/C#min ii-V-I", {"F#min7,B7,Emaj7,Amaj7,D#min7b5,G#7,C#min7"}},
+    {"Amaj/F#min ii-V-I", {"Bmin7,E7,Amaj7,Dmaj7,G#min7b5,C#7,F#min7"}},
+    {"Dmaj/Bmin ii-V-I",  {"Emin7,A7,Dmaj7,Gmaj7,C#min7b5,F#7,Bmin7"}},
+    {"Gmaj/Emin ii-V-I",  {"Amin7,D7,Gmaj7,Cmaj7,F#min7b5,B7,Emin7"}}
+};
+
+template<typename K, typename V>
+std::vector<K> mapKeys(std::map<K, V> inputMap) {
+    std::vector<K> keys;
+    for (const std::pair<K, V> &pair : inputMap) {
+        keys.push_back(pair.first);
+    }
+    return keys;
+}
 
 std::vector<std::string> splitString(std::string s, std::string separator) {
     std::vector<std::string> result;
@@ -19,6 +46,20 @@ std::vector<std::string> splitString(std::string s, std::string separator) {
     }
     
     return result;
+}
+
+std::string joinString(const std::vector<std::string>& strings, const std::string& delimiter) {
+    if (strings.empty()) return "";
+
+    std::ostringstream oss;
+    for (size_t i = 0; i < strings.size(); ++i) {
+        if (i != 0) {
+            oss << delimiter;
+        }
+        oss << strings[i];
+    }
+
+    return oss.str();
 }
 
 template<typename T>
@@ -39,7 +80,7 @@ void printVector(const std::vector<std::vector<T>>& vec, std::string end = "\n")
     std::cout << "]" << "\n";
 }
 
-std::vector<int> parseChord(std::string string){
+Chord parseChord(std::string string){
     enum semitones {
         minor3rd = 3,
         major3rd = 4,
@@ -65,14 +106,14 @@ std::vector<int> parseChord(std::string string){
         {"B",  11}, {"Cb", 11}
     };
 
-    static std::map<std::string, std::function<std::vector<int>(int)>> chordMap = {
-      {"maj", [](int x) { return std::vector<int>{x, x + major3rd, x + perfect5th}; }},
-      {"min", [](int x) { return std::vector<int>{x, x + minor3rd, x + perfect5th}; }},
-      {"7", [](int x) { return std::vector<int>{x, x + major3rd, x + perfect5th, x + minor7th}; }},
-      {"maj7", [](int x) { return std::vector<int>{x, x + major3rd, x + perfect5th, x + major7th}; }},
-      {"min7", [](int x) { return std::vector<int>{x, x + minor3rd, x + perfect5th, x + minor7th}; }},
-      {"dim7", [](int x) { return std::vector<int>{x, x + minor3rd, x + diminished5th, x + major6th}; }},
-      {"min7b5", [](int x) { return std::vector<int>{x, x + minor3rd, x + diminished5th, x + minor7th}; }}
+    static std::map<std::string, std::function<Chord(int)>> chordMap = {
+      {"maj", [](int x) { return Chord{x, x + major3rd, x + perfect5th}; }},
+      {"min", [](int x) { return Chord{x, x + minor3rd, x + perfect5th}; }},
+      {"7", [](int x) { return Chord{x, x + major3rd, x + perfect5th, x + minor7th}; }},
+      {"maj7", [](int x) { return Chord{x, x + major3rd, x + perfect5th, x + major7th}; }},
+      {"min7", [](int x) { return Chord{x, x + minor3rd, x + perfect5th, x + minor7th}; }},
+      {"dim7", [](int x) { return Chord{x, x + minor3rd, x + diminished5th, x + major6th}; }},
+      {"min7b5", [](int x) { return Chord{x, x + minor3rd, x + diminished5th, x + minor7th}; }}
     };
     
     static std::regex pattern("(^[A-Za-z]#?b?)(.*)$");
@@ -90,10 +131,10 @@ std::vector<int> parseChord(std::string string){
     }
 }
 
-std::vector<std::vector<int>> parseChords(std::vector<std::string> chords) {
-    std::vector<std::vector<int>> allChordNotes;
+std::vector<Chord> parseChords(std::vector<std::string> chords) {
+    std::vector<Chord> allChordNotes;
     for (std::string chord : chords) {
-        std::vector<int> chordNotes = parseChord(chord);
+        Chord chordNotes = parseChord(chord);
         allChordNotes.push_back(chordNotes);
     }
     return allChordNotes;
@@ -101,23 +142,39 @@ std::vector<std::vector<int>> parseChords(std::vector<std::string> chords) {
 
 std::string activeNotesString() {
     std::string result;
-    static std::array<std::string, 12> noteSymbols = {"c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"};
     for (size_t i = 0; i < PIANONOTESACTIVE.size(); ++i) {
         if (PIANONOTESACTIVE[i]) {
-            result += noteSymbols[i%12] + " ";
+            result += NOTE_SYMBOLS[i%12] + " ";
         }
     }
     return result;
 }
 
-std::vector<int> removeActiveNotes(std::vector<int>& chord, std::array<bool,36>& activeNotes) {
-    std::vector<int> result;
+std::string getChordString(Chord chord){
+    std::string result;
+    for (int note : chord) {
+        result += NOTE_SYMBOLS[note%12] + " ";
+    }
+    return result;
+}
+
+Chord removeActiveNotes(Chord& chord, std::array<bool,36>& activeNotes) {
+    Chord result;
     for (int i : chord){
         if (!activeNotes[i]){
             result.push_back(i);
         }
     }
     return result;
+} 
+
+bool compareNotes(Chord& chord, std::array<bool,36>& activeNotes) {
+    for (int i : chord){
+        if (!activeNotes[i]){
+            return false;
+        }
+    }
+    return true;
 } 
 
 static void midiCallback(double deltatime, std::vector<unsigned char>* message, void* userData) {
@@ -141,7 +198,21 @@ RtMidiIn* initMidi(){
   }
 }
 
-void drawPiano(Vector2 &windowSize, std::vector<int> &blueChord){
+void dummyPcKeyboard(std::array<bool, 36> &PIANONOTESACTIVE){
+    static int keys[20] = { 
+        KEY_A, KEY_W, KEY_S, KEY_E, KEY_D, KEY_F, KEY_T, KEY_G, KEY_Y, KEY_H, 
+        KEY_U, KEY_J, KEY_K, KEY_O, KEY_L, KEY_P, KEY_SEMICOLON, KEY_APOSTROPHE, 
+        KEY_RIGHT_BRACKET, KEY_BACKSLASH 
+    };
+    for (int i = 0; i < 20; i++) {
+        if (IsKeyDown(keys[i]))
+            PIANONOTESACTIVE[i] = true;
+        else if (IsKeyUp(keys[i]))
+            PIANONOTESACTIVE[i] = false;
+    }
+}
+
+void drawPiano(Vector2 &windowSize, Chord &blueChord){
     Color color;
     float size = 25.0f;
     int numOctaves = 3;
@@ -194,22 +265,54 @@ void drawPiano(Vector2 &windowSize, std::vector<int> &blueChord){
 
     for (int i : whiteKeys) {
         color = RAYWHITE;
-        if (PIANONOTESACTIVE[i]) color = RED;
-        else if ((std::find(blueChord.begin(), blueChord.end(), i) != blueChord.end())) color = BLUE;
+        bool isInBlueChord = std::find(blueChord.begin(), blueChord.end(), i) != blueChord.end();
+        if (PIANONOTESACTIVE[i] && isInBlueChord) color = GREEN;
+        else if (!PIANONOTESACTIVE[i] && isInBlueChord) color = BLUE;
+        else if (PIANONOTESACTIVE[i]) color = RED;
         DrawRectangleRec(shapes.at(i), color);
         DrawRectangleLinesEx(shapes.at(i), 2, BLACK);
     }
 
     for (int i : blackKeys) {
         color = BLACK;
-        if (PIANONOTESACTIVE[i]) color = RED;
-        else if ((std::find(blueChord.begin(), blueChord.end(), i) != blueChord.end())) color = BLUE;
+        bool isInBlueChord = std::find(blueChord.begin(), blueChord.end(), i) != blueChord.end();
+        if (PIANONOTESACTIVE[i] && isInBlueChord) color = GREEN;
+        else if (!PIANONOTESACTIVE[i] && isInBlueChord) color = BLUE;
+        else if (PIANONOTESACTIVE[i]) color = RED;
         DrawRectangleRec(shapes.at(i), color);
         DrawRectangleLinesEx(shapes.at(i), 2, BLACK);
     }
 }
 
-void renderInputScene(Vector2 &windowSize, char (&input)[256], bool &textBoxEditable, std::vector<std::vector<int>> &parsedChordsResult, Scene &currentScene){
+void playPianoSounds(std::array<bool,36> &activeNotes, std::array<bool,36> &pastActiveNotes, std::array<Music,36> pianoSounds){
+    for(size_t i = 0; i < activeNotes.size(); i++){
+        if (!(pastActiveNotes[i]) && activeNotes[i]){
+            if (IsMusicStreamPlaying(pianoSounds[i])) {
+                StopMusicStream(pianoSounds[i]);
+            }
+            PlayMusicStream(pianoSounds[i]);
+        }
+    }
+}
+
+void parseInputString(
+        char *input,
+        std::vector<Chord> &parsedChordsResult
+    ){
+    std::vector<std::string> chords = splitString((std::string)input, ",");
+    parsedChordsResult = parseChords(chords);
+}
+
+void renderInputScene(
+        bool &loop,
+        Vector2 &windowSize,
+        char (&input)[256],
+        bool &textBoxEditable,
+        int &radioScrollIndex,
+        int &radioActiveIndex,
+        std::vector<Chord> &parsedChordsResult,
+        Scene &currentScene
+    ){
     char text[] = "Enter a list of chords:";
     DrawText(text, windowSize.x * 0.2f, windowSize.y * 0.4f, 20, LIGHTGRAY);
     
@@ -222,28 +325,47 @@ void renderInputScene(Vector2 &windowSize, char (&input)[256], bool &textBoxEdit
     
     if (IsKeyDown(KEY_ENTER)) {
         currentScene = SCENE_PIANO;
+        parseInputString(input, parsedChordsResult);
+    }
 
-        std::vector<std::string> chords = splitString((std::string)input, ",");
-        parsedChordsResult = parseChords(chords);
+    std::vector<std::string> optionsKeys = mapKeys(OPTIONS);
+    optionsKeys.insert(optionsKeys.begin(), "User Input");
+    std::string optionsString = joinString(optionsKeys,";");
+
+    GuiListView((Rectangle){windowSize.x * 0.3f, windowSize.y * 0.6f, 300, 100}, optionsString.c_str(), &radioScrollIndex, &radioActiveIndex);
+
+    GuiCheckBox((Rectangle){windowSize.x * 0.3f, windowSize.y * 0.3f, 20, 20}, "Loop", &loop);
+
+    if (radioActiveIndex != 0){
+        strcpy(input, OPTIONS[optionsKeys[radioActiveIndex + 1]].data());
     }
 }
 
-void renderPianoScene(Vector2 &windowSize, char (&input)[256], std::vector<std::vector<int>> &parsedChords, Scene &currentScene){
-    std::vector<int>& chord = parsedChords.front();
-    std::cout << "These are the parsedChords\n";
-    printVector(parsedChords);
-    std::cout << "This is the current chord\n";
-    printVector(chord);
 
+
+void renderPianoScene(
+        Vector2 &windowSize,
+        bool loop,
+        char (&input)[256],
+        std::vector<Chord> &parsedChords,
+        std::array<Music,36> pianoSounds,
+        std::array<bool,36> lastPIANONOTESACTIVE,
+        Scene &currentScene
+    ){
+    Chord& chord = parsedChords.front();
     drawPiano(windowSize,chord);
+    playPianoSounds(PIANONOTESACTIVE,lastPIANONOTESACTIVE,pianoSounds);
+
     DrawText(activeNotesString().c_str(), windowSize.x * 0.2f, windowSize.y * 0.4f, 20, LIGHTGRAY);
+    DrawText(getChordString(chord).c_str(), windowSize.x * 0.7f, windowSize.y * 0.4f, 20, DARKBLUE);
 
-    chord = removeActiveNotes(chord,PIANONOTESACTIVE);
-
-    if (chord.empty() && !parsedChords.empty()){
+    if(compareNotes(chord,PIANONOTESACTIVE) && !parsedChords.empty()){
         parsedChords.erase(parsedChords.begin());
+
         if (!parsedChords.empty()){
             chord = parsedChords.front();
+        } else if (loop){
+            parseInputString(input, parsedChords);
         } else {
             std::cout << "Progression finished, please provide more chords\n";
             currentScene = SCENE_INPUT;
@@ -251,30 +373,105 @@ void renderPianoScene(Vector2 &windowSize, char (&input)[256], std::vector<std::
     }
 }
 
+
 int main() {
-    bool textBoxEditable;
+    // Initialize variables
+    bool loop = false;
+    int radioScroll = 0;
+    int radioActive = 0;
+    bool textBoxEditable = true;
     RtMidiIn* midiin = initMidi();
+    std::vector<Chord> parsedChords;
     Vector2 windowSize = {800, 450};
     Scene currentScene = SCENE_INPUT;
     char input[256] = "Dmin7,Cmaj7,Fmaj7";
-    std::vector<std::vector<int>> parsedChords;
+    std::array<bool, 36> lastPIANONOTESACTIVE = {0};
 
+    // Initialize window and audio
     SetTargetFPS(60);
     InitWindow(windowSize.x, windowSize.y, "Piano");
-    
+    InitAudioDevice();
 
-    while (!WindowShouldClose()){
+    // Define piano sound file paths
+    const std::array<const char*, 12> soundFiles = {
+        "/Users/user/repos/cefa/src/assets/c1.wav",
+        "/Users/user/repos/cefa/src/assets/c1s.wav",
+        "/Users/user/repos/cefa/src/assets/d1.wav",
+        "/Users/user/repos/cefa/src/assets/d1s.wav",
+        "/Users/user/repos/cefa/src/assets/e1.wav",
+        "/Users/user/repos/cefa/src/assets/f1.wav",
+        "/Users/user/repos/cefa/src/assets/f1s.wav",
+        "/Users/user/repos/cefa/src/assets/g1.wav",
+        "/Users/user/repos/cefa/src/assets/g1s.wav",
+        "/Users/user/repos/cefa/src/assets/a1.wav",
+        "/Users/user/repos/cefa/src/assets/a1s.wav",
+        "/Users/user/repos/cefa/src/assets/b1.wav"
+    };
+
+    // Load piano sounds
+    std::array<Music, 36> pianoSounds;
+    for(int octave = 0; octave < 3; octave++) {
+        for(int note = 0; note < 12; note++) {
+            int index = octave * 12 + note;
+            pianoSounds[index] = LoadMusicStream(soundFiles[note]);
+            pianoSounds[index].looping = false;
+            SetMusicPitch(pianoSounds[index], 1.0f * std::pow(2.0f, octave));
+        }
+    }
+
+    // Main game loop
+    while (!WindowShouldClose()) {
+        // Update audio streams
+        for (auto& sound : pianoSounds) {
+            UpdateMusicStream(sound);
+        }
+
+        // Handle keyboard input
+        dummyPcKeyboard(PIANONOTESACTIVE);
+
+        // Render
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        switch (currentScene){
+        // Scene management
+        switch (currentScene) {
             case SCENE_INPUT:
-                renderInputScene(windowSize,input,textBoxEditable,parsedChords,currentScene);
+                renderInputScene(
+                    loop,
+                    windowSize,
+                    input,
+                    textBoxEditable,
+                    radioScroll,
+                    radioActive,
+                    parsedChords,
+                    currentScene
+                );
                 break;
             case SCENE_PIANO:
-                renderPianoScene(windowSize,input,parsedChords,currentScene);
+                renderPianoScene(
+                    windowSize,
+                    loop,
+                    input,
+                    parsedChords,
+                    pianoSounds,
+                    lastPIANONOTESACTIVE,
+                    currentScene
+                );
                 break;
         }
+
         EndDrawing();
+        
+        // Store current piano state
+        std::copy(PIANONOTESACTIVE.begin(), PIANONOTESACTIVE.end(), lastPIANONOTESACTIVE.begin());
     }
+
+    // Cleanup
+    for (auto& sound : pianoSounds) {
+        UnloadMusicStream(sound);
+    }
+    CloseAudioDevice();
+    CloseWindow();
+
+    return 0;
 }
